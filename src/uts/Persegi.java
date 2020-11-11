19c0f6cf2011f6d52d9a4d0403f2289970d78699
mokhamadisna <mokhammadisna@gmail.com>
2020-11-11 15:50:09 +0700

    publish all

---
+++ src/uts/Persegi.java
@@ -0,0 +1,17 @@
+package uts;
+
+public class Persegi {
+    public static void main (String[] args){
+        int sisi;
+        int hasilkeliling;
+        int hasilluas;
+
+        sisi=32;
+
+        hasilkeliling = sisi*4;
+        hasilluas = sisi*sisi;
+
+        System.out.println("Hasil Keliling Persegi : " +hasilkeliling);
+        System.out.println("Hasil Keliling Persegi : " +hasilluas);
+    }
+}
